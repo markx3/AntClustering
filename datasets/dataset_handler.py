90a8767287904ca22b4e24acfcc9dd87f19feab6
Marcos Felipe Eipper <marcos@eipper.com.br>
2017-09-05 09:44:15 -0300

    Deleted some files

--- datasets/dataset_handler.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-info = np.loadtxt('breast-cancer-wisconsin.txt', delimiter=',')
-np.savetxt('breast-cancer-wisconsin2.txt', info[:,1:], delimiter="\t",
-            fmt="%.5f", )
